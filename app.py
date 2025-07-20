7c206c45f74ba3ef844b6f02cd9d7623a3a05292
Nathan <nathanrossi30@gmail.com>
2025-07-20 19:39:23 -0300

    teste

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, redirect, url_for, request;
 import csv
 
+#ren "C:\Users\bta\AppData\Local\Microsoft\WindowsApps\python.exe" python_disabled.exe
+#set PATH=%PATH%;C:\ProgramData\anaconda3;C:\ProgramData\anaconda3\Scripts;C:\ProgramData\anaconda3\Library\bin 
+
 app = Flask(__name__)
 
-app.get("/add")
+app.route("/")
 def index():
-  return render_template("index.html")
+  return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
